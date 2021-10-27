0b4abfdd874a5666a2c7135984fcaea46ea7418e
Nikolai <34409368+PNikolaiAlzate@users.noreply.github.com>
2021-10-27 07:12:32 -0500

    New Files

---
+++ reading_writing_files.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+
+#x = Path('spam', 'bacon', 'eggs')
+
+#print(str(x))
+
+#y = Path.cwd()
+#print(y)
+
+#os.chdir('/Users/pnalzate/Documents/GitHub')
+
+#print(Path.cwd())
+
+os.makedirs("/Users/pnalzate/Desktop/New_Directory/")
