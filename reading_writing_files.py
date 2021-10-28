efb6e29e5e464392bd58c063a4fc1faae3a8e0ba
Nikolai <34409368+PNikolaiAlzate@users.noreply.github.com>
2021-10-28 15:15:34 -0500

    Added Code

--- reading_writing_files.py
+++ reading_writing_files.py
@@ -1,15 +1,19 @@
-from pathlib import Path
-import os
+# Usage: py.exe mcb.pyw save <keyword> - Saves clipboard to keyword.
+#        py.exe mcb.pyw <keyword> - Loads keyword to clipboard.
+#        py.exe mcb.pyw list - Loads all keywords to clipboard.
 
-#x = Path('spam', 'bacon', 'eggs')
+import shelve, pyperclip, sys
 
-#print(str(x))
+mcbShelf = shelve.open('mcb')
 
-#y = Path.cwd()
-#print(y)
+# TODO: Save clipboard content.
+if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
+    mcbShelf[sys.argv[2]] = pyperclip.paste()
+elif len(sys.argv) == 2:
+    # TODO: List keywords and load content.
+    if sys.argv[1].lower() == 'list':
+        pyperclip.copy(str(list(mcbShelf.keys())))
+    elif sys.argv[1] in mcbShelf:
+        pyperclip.copy(mcbShelf[sys.argv[1]])
 
-#os.chdir('/Users/pnalzate/Documents/GitHub')
-
-#print(Path.cwd())
-
-os.makedirs("/Users/pnalzate/Desktop/New_Directory/")
+mcbShelf.close()
